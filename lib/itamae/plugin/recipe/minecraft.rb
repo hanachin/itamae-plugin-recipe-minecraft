d5d335ad6cdff34b4918b41e6af3e8db35639c18
Seiei Higa <hanachin@gmail.com>
2014-12-27 13:33:33 +0900

    initial commit

---
+++ lib/itamae/plugin/recipe/minecraft.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/minecraft/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Minecraft
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/minecraft/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module Minecraft
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
