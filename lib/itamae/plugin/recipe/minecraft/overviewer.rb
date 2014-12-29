ac00e2aaf74b4761b9790b21d638696126f8606b
Seiei Higa <hanachin@gmail.com>
2014-12-29 16:49:25 +0900

    add overviewer.org to apt-key, then apt-update

--- lib/itamae/plugin/recipe/minecraft/overviewer.rb
+++ lib/itamae/plugin/recipe/minecraft/overviewer.rb
@@ -1,6 +1,15 @@
 execute 'add overviewer to apt sources.list' do
-  command 'echo "deb http://overviewer.org/debian ./" >> /etc/apt/sources.list && apt-get update'
+  command 'echo "deb http://overviewer.org/debian ./" >> /etc/apt/sources.list'
   not_if 'cat /etc/apt/sources.list | grep http://overviewer.org/debian'
+end
+
+execute 'add apt-key' do
+  command 'wget -O - http://overviewer.org/debian/overviewer.gpg.asc | sudo apt-key add -'
+  not_if 'apt-key list | grep overviewer.org'
+end
+
+execute 'apt-get update' do
+  command 'apt-get update'
 end
 
 package 'minecraft-overviewer'
